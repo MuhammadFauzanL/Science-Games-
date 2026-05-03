34a78276c553f18a779168bf6ebd57bde4303fd4
M Fauzan Lubada <fauzanlubada5@gmail.com>
2026-05-03 16:19:14 +0700

    Initial commit with latest files and auto-update feature

---
+++ scripts/register-sw.js
@@ -0,0 +1,1 @@
+"use strict";window.C3_RegisterSW=async function(){if(navigator.serviceWorker)try{const e=await navigator.serviceWorker.register("sw.js",{scope:"./", updateViaCache: 'none'});console.info("Registered service worker on "+e.scope)}catch(e){console.warn("Failed to register service worker: ",e)}};
